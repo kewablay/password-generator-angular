0d039b45f7505fb4101e64e537cce7f1e6d591aa
kewablay <kojokewajunior123@gmail.com>
2024-07-15 16:23:20 +0000

    added initial setup of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PasswordGeneratorComponent } from './components/password-generator/password-generator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PasswordGeneratorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'password-generator';
